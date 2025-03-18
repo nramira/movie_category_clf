8f99f364ab5993ba61dd1902c7969e74888ed06f
Nicolas <nicolasramirez9802@gmail.com>
2025-03-18 09:26:07 +0100

    Include strings as inputs for error_message_detail

--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,7 @@
 import sys
 
-from src.logger import logging
 
-
-def error_message_detail(error, error_detail: sys):
+def error_message_detail(error, error_detail: sys) -> str:
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = f"Error occured in python script name: [{file_name}]\nLine number [{exc_tb.tb_lineno}]\nError message [{str(error)}]"
@@ -12,17 +10,9 @@ def error_message_detail(error, error_detail: sys):
 
 
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail: sys):
+    def __init__(self, error_message, error_detail: sys) -> None:
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.error_message
-
-
-if __name__ == "__main__":
-    try:
-        a = 1 / 0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomException(e, sys)
